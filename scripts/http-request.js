97bf3eccdea2727f6f605c35d20c0ca88b3300a5
shandytias29@gmail.com <shandytias29@gmail.com>
2019-06-17 12:40:03 +0700

    Change URL API

--- scripts/http-request.js
+++ scripts/http-request.js
@@ -1,15 +1,16 @@
 //Http Request
 
-var webServiceBaseUrl = "http://localhost:10081";
+// var webServiceBaseUrl = "http://localhost:10081";
+var webServiceBaseUrl = "http://172.18.18.65:20081";
 
-mainApp.factory("HttpRequest", function($http, $q) {
-    var get = function(query) {
+mainApp.factory("HttpRequest", function ($http, $q) {
+    var get = function (query) {
         return $http.get(webServiceBaseUrl + query);
     };
-    var post = function(query, data) {
+    var post = function (query, data) {
         return $http.post(webServiceBaseUrl + query, data);
     };
-    var del = function(query) {
+    var del = function (query) {
         return $http.delete(webServiceBaseUrl + query);
     };
 
