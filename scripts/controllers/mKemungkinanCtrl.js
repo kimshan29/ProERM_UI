444f5e2b64ad349f28d18f942b2b608dd51dfae9
shandytias29@gmail.com <shandytias29@gmail.com>
2019-07-11 17:05:32 +0700

    Connect CRUD master kemungkinan dengan API

--- scripts/controllers/mKemungkinanCtrl.js
+++ scripts/controllers/mKemungkinanCtrl.js
@@ -8,6 +8,7 @@ mainApp.controller("mKemungkinanCtrl", function ($scope, $routeParams, $q, $cook
     $scope.kemungkinan.isEditMode = false;
     $scope.master = {};
     $scope.master.tahun = [];
+    $scope.master.peringkatKemungkinan = [];
 
     //Master Tahun
     var years = Helper.generateStackedYears(2018, 2);
@@ -25,9 +26,10 @@ mainApp.controller("mKemungkinanCtrl", function ($scope, $routeParams, $q, $cook
         $scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withOption('responsive', true).withDisplayLength(10);
 
         $scope.renderKemungkinan();
-        $scope.renderKategoriRisiko();
-        $scope.renderSubKategoriRisiko();
-        $scope.renderAreaDampak();
+        $scope.renderTipeKemungkinan();
+        $scope.renderPeringkatKemungkinan();
+
+
     }
 
     $scope.eventClickAdd = function () {
@@ -39,18 +41,19 @@ mainApp.controller("mKemungkinanCtrl", function ($scope, $routeParams, $q, $cook
         NProgress.start();
         $scope.renderKemungkinan();
         $scope.kemungkinan.isEditMode = false;
+        $scope.clearForm();
         NProgress.done();
     }
 
     $scope.eventClickSave = function () {
 
         var apiUrl = "/api/MasterKemungkinan";
-        // $scope.kemungkinan.input.id = "";
-        $scope.kemungkinan.input.userEmail = $scope.currentUser.email;
+        $scope.form.userEmail = $scope.currentUser.email;
 
-        console.log(JSON.stringify($scope.kemungkinan.input));
 
-        HttpRequest.post(apiUrl, $scope.kemungkinan.input).success(function (response) {
+        console.log(JSON.stringify($scope.form));
+
+        HttpRequest.post(apiUrl, $scope.form).success(function (response) {
             $scope.renderKemungkinan();
             $scope.kemungkinan.isEditMode = false;
         });
@@ -60,13 +63,9 @@ mainApp.controller("mKemungkinanCtrl", function ($scope, $routeParams, $q, $cook
         NProgress.start();
         var apiUrl = "/api/MasterKemungkinan/" + id;
 
-
-
-        $scope.kemungkinan.input.userEmail = $scope.currentUser.email;
-
         HttpRequest.get(apiUrl).success(function (response) {
             console.log(JSON.stringify(response));
-            $scope.kemungkinan.input = response;
+            $scope.form = response;
             $scope.kemungkinan.isEditMode = true;
             NProgress.done();
         });
@@ -94,34 +93,43 @@ mainApp.controller("mKemungkinanCtrl", function ($scope, $routeParams, $q, $cook
         var apiUrl = "/api/MasterKemungkinan";
         HttpRequest.get(apiUrl).success(function (response) {
             $scope.kemungkinan.data = response;
-            console.log(JSON.stringify(response));
+            // console.log(JSON.stringify(response));
             NProgress.done();
         });
     }
 
-    $scope.renderKategoriRisiko = function () {
-        var apiUrl = "/api/MasterKategoriRisiko";
+    $scope.renderPeringkatKemungkinan = function () {
+        var apiUrl = "/api/MasterPeringkatKemungkinan";
         HttpRequest.get(apiUrl).success(function (response) {
-            $scope.master.kategoriRisiko = response;
+            $scope.master.peringkatKemungkinan = response;
             console.log(JSON.stringify(response));
+
+        })
+
+    }
+
+    $scope.renderTipeKemungkinan = function () {
+        NProgress.start();
+        var apiUrl = "/api/MasterTipeKemungkinan";
+        HttpRequest.get(apiUrl).success(function (response) {
+            $scope.master.tipeKemungkinan = response;
+            console.log(JSON.stringify(response));
+            // alert("Tipe Kemungkinan")
+            NProgress.done();
         });
     }
 
-    $scope.renderSubKategoriRisiko = function () {
-        var apiUrl = "/api/MasterSubKategoriRisiko";
-        HttpRequest.get(apiUrl).success(function (response) {
-            $scope.master.subKategoriRisiko = response;
-            console.log(JSON.stringify(response));
-        });
+    $scope.clearForm = () => {
+        $scope.form = {
+            peringkatKemungkinan: '',
+            tipeKemungkinan: '',
+            kemungkinan: '',
+            isActive: ''
+
+        }
     }
 
-    $scope.renderAreaDampak = function () {
-        var apiUrl = "/api/MasterAreaDampak";
-        HttpRequest.get(apiUrl).success(function (response) {
-            $scope.master.areaDampak = response;
-            console.log(JSON.stringify(response));
-        });
-    }
+
 
     $scope.formLoad();
 });
